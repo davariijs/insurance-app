c2990924a85d0963cbea02365f56ca75fe003a98
davariijs <Atuor127@gmail.com>
2025-06-06 12:01:50 -0400

    feat: remove unused i18n imports and related code from App and main files

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './context/ThemeContext.tsx';
-import './config/i18n.ts';
 
 const queryClient = new QueryClient();
 
